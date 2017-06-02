33dddd596d987f2bffc410e76f56595df67c779d
romainbrette <romain.brette@inserm.fr>
2017-06-02 17:50:49 +0200

    Display set pressure in patcher GUI

--- patcher.py
+++ patcher.py
@@ -30,36 +30,38 @@ class PatcherApplication(Frame):
         Button(self, text='Seal', command=self.seal).pack()
         Button(self, text='Release', command=self.release).pack()
         Button(self, text='Break in', command=self.break_in).pack()
-
+        self.pressure_label = Label(self, text='')
+        self.pressure_label.pack()
         self.high_pressure() # We start with high pressure
+
+    def update_pressure(self, pressure):
+        self.pressure = pressure
+        self.controller.set_pressure(self.pressure)
+        self.pressure_label['text'] = 'Pressure: {}mbar'.format(self.pressure)
 
     def high_pressure(self):
         if verbose:
             print "High pressure"
-        self.pressure = 800 # mBar (800-1000 in 2012 Kodandaramaiah paper)
-        self.controller.set_pressure(self.pressure)
+        self.update_pressure(800)  # mBar (800-1000 in 2012 Kodandaramaiah paper)
 
     def nearing(self):
         if verbose:
             print "Nearing"
-        self.pressure = 25 # mBar (25-30)
-        self.controller.set_pressure(self.pressure)
+        self.update_pressure(25)  # mBar (25-30)
 
     def seal(self):
         # Sealing with small negative pressure
         if verbose:
             print "Sealing"
-        self.pressure = -25 #(from Desai)
+        self.update_pressure(-25) #(from Desai)
         # -15 to -20 in Kodandaramaiah paper?
-        self.controller.set_pressure(self.pressure)
 
     def release(self):
         # Release the pressure
         if verbose:
             print "Releasing"
-        self.pressure = 0
+            self.update_pressure(0)
         # -15 to -20 in Kodandaramaiah paper?
-        self.controller.set_pressure(self.pressure)
 
     def break_in(self):
         # Breaks in with a ramp
@@ -67,15 +69,14 @@ class PatcherApplication(Frame):
         # Desai: -150 mBar for 1 second; repeated attempts
         if verbose:
             print "Breaking in; release after 1 s"
-        self.pressure = -150
-        self.controller.set_pressure(self.pressure)
+        self.update_pressure(-150)
         self.master.after(1000, self.release)
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Patcher')
 
-    controller = OB1(calibrate=True)
+    controller = OB1()
     app = PatcherApplication(root, controller).pack(side="top", fill="both", expand=True)
     root.mainloop()
 
