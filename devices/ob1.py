c81658a82ac3a4e44ec85dbe9e6cfbf815fadd2a
romainbrette <romain.brette@inserm.fr>
2017-06-02 17:40:22 +0200

    Use correct ID and improve error handling for OB1 pressure controller

--- devices/ob1.py
+++ devices/ob1.py
@@ -12,31 +12,36 @@ from Elveflow64 import *
 
 __all__ = ['OB1']
 
+def _check_error(task, error):
+    if error != 0:
+        raise RuntimeError('{} failed with error code {}'.format(task, error))
 
 class OB1(object):
     def __init__(self, calibrate=False):
         self.instr_ID = c_int32()
         print('Instrument name and regulator types hardcoded in the python script'.encode('utf-8'))
         # see User guide to determine regulator type NI MAX to determine the instrument name
-        error = OB1_Initialization('01868D03'.encode('ascii'), 4, 0, 0, 0, byref(self.instr_ID))
+        error = OB1_Initialization('01C1690E'.encode('ascii'), 4, 0, 0, 0, byref(self.instr_ID))
         # all functions will return error code to help you to debug your code, for further information see user guide
-        print('error:%d' % error)
-        print('OB1 ID: %d' % self.instr_ID.value)
+        _check_error('Initialization', error)
 
         # add one analog flow sensor
         error = OB1_Add_Sens(self.instr_ID, 1, 4, 0, 1)
-        print('error add analog flow sensor:%d' % error)
+        _check_error('Adding analog flow sensor', error)
 
         calib_path = os.path.expanduser(r'~\ob1_calibration.txt')
         self.calib = (c_double * 1000)()
         if calibrate:
+            print ('Starting calibration')
             OB1_Calib(self.instr_ID.value, self.calib, 1000)
             error = Elveflow_Calibration_Save(calib_path.encode('ascii'), byref(self.calib), 1000)
-            print ('calib saved in %s' % calib_path.encode('ascii'))
+            print ('Calibration finished')
+            print ('Calibration saved in file %s' % calib_path.encode('ascii'))
         else:
             if not os.path.isfile(calib_path):
                 raise IOError('Calibration file "{}" does not exist'.format(calib_path))
             error = Elveflow_Calibration_Load(calib_path.encode('ascii'), byref(self.calib), 1000)
+            _check_error('Loading calibration file', error)
 
 
     def measure(self, port=0):
@@ -47,7 +52,7 @@ class OB1(object):
         set_channel = c_int32(port)  # convert to c_int32
         get_pressure = c_double()
         error =  OB1_Get_Sens_Data(self.instr_ID.value, set_channel, 1, byref(get_pressure))  # Acquire_data =1 -> Read all the analog value
-        print('error (get_pressure): ', error)
+        _check_error('Getting data from flow sensor', error)
         return get_pressure.value
 
     def set_pressure(self, pressure, port=0):
@@ -59,4 +64,4 @@ class OB1(object):
         set_pressure = float(pressure)
         set_pressure = c_double(pressure)  # convert to c_double
         error = OB1_Set_Press(self.instr_ID.value, set_channel, set_pressure, byref(self.calib), 1000)
-        print('error: (set_pressure) ', error)
+        _check_error('Setting pressure', error)
